a9f6f8f7c5bcffc1ed467b661ad8372ccff07c03
ashurohom <ashitosh.rohom@gmail.com>
2025-11-21 17:29:51 +0530

    update addtask.jsx

--- src/pages/AddTask.jsx
+++ src/pages/AddTask.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 export default function AddTask({ onNavigate }) {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [status, setStatus] = useState("");
+  const [priority, setPriority] = useState("");
+  const [message, setMessage] = useState("");     // For showing messages
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();     //stop page reload
+
+    const newTask ={
+      title: title,
+      description: description,
+      dueDate: dueDate,
+      status: status,
+      priority: priority, 
+    };
+
+    
+  }
+
+
   return (
     <div>
       <h1>Add Task Page</h1>
